0bf6dc3e678f4cc8e7caba875184e908c4d2058e
deep-singhhh <161069474+deep-singhhh@users.noreply.github.com>
2024-02-24 09:50:54 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"physics"<<endl<<"wallah";
+    return 0;
+}
---
+++ q5.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<2*(22/7.0)*10;
+    return 0;
+}
